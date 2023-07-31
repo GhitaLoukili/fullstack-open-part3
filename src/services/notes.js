3b5282176d522e3513015574e9370ba8d56784b3
Ghita <85077489+Ghita-L28@users.noreply.github.com>
2023-07-31 17:56:04 +0100

    3.12

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,17 +1,20 @@
 import axios from 'axios'
 const baseUrl = '/api/notes'
 
-export const getAll = () => {
+const getAll = () => {
   const request=axios.get(baseUrl)
   return request.then(response=>response.data)
 }
 
-export const create = newObject => {
+const create = newObject => {
   const request= axios.post(baseUrl, newObject)
   return request.then(response=>response.data)
 }
 
-export const update = (id, newObject) => {
+const update = (id, newObject) => {
   const request= axios.put(`${baseUrl}/${id}`, newObject)
   return request.then(response=>response.data)
 }
+
+const exportedObject = { getAll, create, update }
+export default exportedObject
