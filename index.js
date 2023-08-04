d5e471fb7a02fc5ab6b9c34d6ef4879ac187567b
Ghita <85077489+Ghita-L28@users.noreply.github.com>
2023-08-04 14:29:38 +0100

    3.19-3.22

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(requestLogger)
 app.use(express.static('build'))
 
 
-app.get('/api/notes',(request,response)=>{
+app.get('/api/notes',(request,response) => {
   Note.find({}).then(notes => {
     response.json(notes)
   })
@@ -47,8 +47,7 @@ app.get('/api/notes/:id', (request, response,next) => {
       if (note) {
         response.json(note)
       } else {
-        response.status(404).end() 
-      }
+        response.status(404).end() }
     })
     .catch(error => next(error))
 })
@@ -64,19 +63,19 @@ app.post('/api/notes', (request, response,next) => {
   note.save().then(savedNote => {
     response.json(savedNote)
   })
-  .catch(error=> next(error))
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndRemove(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const {content,important} = request.body
+  const { content,important } = request.body
 
   Note.findByIdAndUpdate(request.params.id,{ content, important },{ new: true, runValidators: true, context: 'query' })
     .then(updatedNote => {
